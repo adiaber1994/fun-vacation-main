f27445c691f1d18d8a2e162dddd19bf3c2aedd11
adiaber1994 <adiaber1994@gmail.com>
2023-06-12 06:47:17 +0300

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,34 @@
+import Title from "../components/Title";
+
 function Home() {
-  return <>Order</>;
+  return (
+    <>
+      <Title />
+      <div className="d-flex">
+        <div>
+          <input
+            type="text"
+            placeholder="Serch"
+            className="form-control me-4"
+          />
+        </div>
+        <div>
+          <select className="form-select">
+            <option>--</option>
+          </select>
+        </div>
+      </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Location</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+      </table>
+    </>
+  );
 }
 
 export default Home;
